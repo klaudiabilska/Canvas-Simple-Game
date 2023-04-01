796eff06010fc40052d9d8cdd93c71a6246a7bcd
klaudiabilska <klaudiabilska1@gmail.com>
2023-04-01 22:05:34 +0200

    edit

--- app.js
+++ app.js
@@ -42,15 +42,45 @@ class Projectile {
         c.fill()
     }
 
-    
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
 }
 
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
-player.draw()
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+
+}
 
 addEventListener("click", (event) => {
-    const projectile = new Projectile(x, y)
+    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+
+    projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
 })
+
+    
+
+animate()
+
+// https://www.youtube.com/watch?v=eI9idPTT0c4&list=PLpPnRKq7eNW16Wq1GQjQjpTo_E0taH0La&index=1
+
+// createenemies
