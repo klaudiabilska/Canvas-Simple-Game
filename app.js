48ec91b587da1b945168e8dce1c89c62f3d5c717
klaudiabilska <klaudiabilska1@gmail.com>
2023-04-02 22:11:22 +0200

    edit

--- app.js
+++ app.js
@@ -49,12 +49,63 @@ class Projectile {
     }
 }
 
+// enemies
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
-
 const player = new Player(x, y, 30, 'blue')
-
 const projectiles = []
+const enemies = []
+
+function spawnEnemies() {
+    setInterval(() => {
+        const radius = Math.random() * (30 - 4) + 4;
+
+
+        let x 
+        let y
+        if (Math.random() < 0.5) {
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+        y = Math.random() * canvas.height
+        } else {
+            x = Math.random() *  canvas.width
+            y  = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+        const color = 'green'
+        const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+        const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+        }
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+
+        console.log(enemies)
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
@@ -62,6 +113,19 @@ function animate() {
     player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
+    })
+
+    enemies.forEach((enemy, index) => {
+        enemy.update()
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+            if (dist - enemy.radius - projectile.radius < 1) {
+                enemies.splice(index, 1)
+                projectiles.splice(projectileIndex, 1)
+            }
+        });
     })
 
 }
@@ -80,7 +144,5 @@ addEventListener("click", (event) => {
     
 
 animate()
+spawnEnemies()
 
-// https://www.youtube.com/watch?v=eI9idPTT0c4&list=PLpPnRKq7eNW16Wq1GQjQjpTo_E0taH0La&index=1
-
-// createenemies
